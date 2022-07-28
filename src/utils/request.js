e566bacdac4d7369f23f48a0180516a7b1423ab0
itcast <>
2022-07-28 17:53:27 +0800

    set11

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import proxy from '../config/proxy';
 
 const env = import.meta.env.MODE || 'development';
 
-const host = env === 'mock' ? 'https://mock.boxuegu.com/mock/3359' : proxy[env].host; // 如果是mock模式 就不配置host 会走本地Mock拦截
+// const host = env === 'mock' ? 'https://mock.boxuegu.com/mock/3359' : proxy[env].host; // 如果是mock模式 就不配置host 会走本地Mock拦截
 // const host = 'http://172.17.2.134/api-test';
 
 const CODE = {
